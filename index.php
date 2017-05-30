3bb6eb476e126eccecb8fae29d7fdf365b9d6364
Tarun Jangra <tarun@izap.in>
2017-05-30 17:53:24 +0530

    added new files to bootstrap

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
+<?php
+include('bootstrap.php');
+
+$mysession = new Session();
+$mysession->push(array('newvar' => 'ekta'));
+$mysession->pull('newvar');
+
+?>
+
 <h1>Welcome to new learning project</h1>
---
+++ libs/database.php
@@ -0,0 +1,6 @@
+<?php
+// Create Database class
+// it should allow me to connect and select db using constructor
+// it should have execute method which is capable to run all my sql queries.
+// It should directly get me results of my table in the form of objects.
+// 
